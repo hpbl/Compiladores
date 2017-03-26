8cce3c95cf4aae2019a826d506a8cf29533be887
Hilton Pintor <hpbl@cin.ufpe.br>
2017-03-26 16:21:36 -0300

    Corrigindo Inteiro

--- Analisador Léxico/src/br/com/hpbl/analisadorlexico/LinguagemTeste.java
+++ Analisador Léxico/src/br/com/hpbl/analisadorlexico/LinguagemTeste.java
@@ -7,7 +7,7 @@ public class LinguagemTeste {
 
     public static void main(String[] args) throws IOException {
 
-        String expr = "01";
+        String expr = "012309 0 123";
 
         AnalisadorLexico lexical = new AnalisadorLexico(new StringReader(expr));
         lexical.yylex();
